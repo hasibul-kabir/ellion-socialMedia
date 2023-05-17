58bdca651f5698992d7259fd622a846f4e7dcc0c
reshat <reshat.kabir55@gmail.com>
2023-05-17 23:46:36 +0600

    delete post image from node server using fs.unlink

--- controllers/posts/deletePost.js
+++ controllers/posts/deletePost.js
@@ -1,9 +1,12 @@
 const Post = require("../../models/Post");
+const fs = require('fs')
 
 const deletePost = async (req, res) => {
     try {
         const { id } = req.params;
-        await Post.findByIdAndDelete(id)
+        const postData = await Post.findById(id);
+        await Post.findByIdAndDelete(id);
+        fs.unlinkSync(`public/assets/${postData?.picturePath}`)
         res.status(200).json({ message: "Post has been deleted." })
     } catch (error) {
         res.status(500).json({ message: error.message })
